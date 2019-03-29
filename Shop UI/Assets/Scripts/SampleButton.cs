c69fb0f7660742ca12a63b1a2d54581a9879f10e
ijl0322 <ijl0322@gmail.com>
2019-03-29 15:06:48 -0400

    cache

---
+++ Shop UI/Assets/Scripts/SampleButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour
+{
+    public Button button;
+    public Text nameLabel;
+    public Text priceLabel;
+    public Image iconImage;
+
+    private Item item;
+    private ShopScrollList scrollList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    public void Setup(Item currentItem, ShopScrollList currentScrollList)
+    {
+        item = currentItem;
+        nameLabel.text = item.itemName;
+        priceLabel.text = item.price.ToString();
+        iconImage.sprite = item.icon;
+
+        scrollList = currentScrollList;
+
+    }
+}
---
+++ Shop UI/Assets/Scripts/ShopScrollList.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite icon;
+    public float price = 1f;
+}
+
+public class ShopScrollList : MonoBehaviour
+{
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public ShopScrollList otherShop;
+    public Text myGoldDisplay;
+    public SimpleObjectPool buttonObjectPool;
+    public float gold = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshDisplay();
+    }
+
+    public void RefreshDisplay()
+    {
+        AddButton();
+    }
+
+    private void AddButton()
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            Item item = itemList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+            newButton.transform.SetParent(contentPanel);
+
+            SampleButton sampleButton = newButton.GetComponent<SampleButton>();
+            sampleButton.Setup(item, this);
+        }
+    }
+}
